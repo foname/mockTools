ed533236ff1afefb068ececb898c1527e37a1b6e
Fon <chanaphan.prasomwong@gmail.com>
2016-11-23 01:03:21 +0700

    Add GET/POST/PUT to function

--- main_app.py
+++ main_app.py
@@ -1,22 +1,46 @@
 import os
 from flask import Flask, render_template
 from pymongo import MongoClient
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 client = MongoClient(
-    os.environ.get('DB_PORT_27017_TCP_ADDR','localhost'),
+    os.environ.get('DB_PORT_27017_TCP_ADDR','localhost'), 
     27017)
 
-db = client.requests
+db = client.mock_provider
+
 
 @app.route('/')
-def todo():
-
+def list():
     _items = db.requests.find()
     items = [item for item in _items]
-
     return render_template('template.html', items=items)
 
+
+class adapter(Resource):
+    def get(self, provider_slug):
+        item = db.requests.find_one({"request.urlPathMatching": "/{}".format(provider_slug), "request.method":"GET"})
+        if item:
+            print(type(item['response']['status']))
+            return item['response']['jsonBody'], item['response']['status']
+        return
+
+    def post(self, provider_slug):
+        item = db.requests.find_one({"request.urlPathMatching": "/{}".format(provider_slug), "request.method":"POST"})
+        if item:
+            return item['response']['jsonBody'], item['response']['status']
+        return 
+
+    def put(self, provider_slug):
+        item = db.requests.find_one({"request.urlPathMatching": "/{}".format(provider_slug), "request.method":"PUT"})
+        if item:
+            return item['response']['jsonBody'], item['response']['status']
+        return 
+
+api.add_resource(adapter, '/<string:provider_slug>')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
