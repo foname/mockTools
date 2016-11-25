9db20ea840c55d3608793e1d7ee1d881d66c781b
Chanaphan <chanaphan@acommerce.asia>
2016-11-25 18:24:37 +0700

    resolve conflict

--- main_app.py
+++ main_app.py
@@ -8,16 +8,17 @@ app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient(
-    os.environ.get('DB_PORT_27017_TCP_ADDR','localhost'), 
+    os.environ.get('DB_PORT_27017_TCP_ADDR', 'localhost'),
     27017)
-
 db = client.mock_provider
 
 
 @app.route('/')
 def list():
+    print(os.environ.get('DB_PORT_27017_TCP_ADDR', 'localhost'))
     _items = db.requests.find()
     items = [item for item in _items]
+    print(items)
     return render_template('template.html', items=items)
 
 
@@ -33,18 +34,18 @@ class adapter(Resource):
         item = db.requests.find_one({"request.urlPathMatching": "/{}".format(provider_slug), "request.method":"POST"})
         if item:
             return item['response']['jsonBody'], item['response']['status']
-        return 
+        return
 
     def put(self, provider_slug):
         item = db.requests.find_one({"request.urlPathMatching": "/{}".format(provider_slug), "request.method":"PUT"})
         if item:
             return item['response']['jsonBody'], item['response']['status']
-        return 
+        return
+
 
 
 api.add_resource(mirror, '/mirror/<string:provider_slug>')
 api.add_resource(adapter, '/<string:provider_slug>')
-
 
 
 if __name__ == "__main__":
